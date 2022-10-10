986f88c90de1da97c71c9c6ac10ab8665af227d3
Storas008 <naglis.kontautas04@gmail.com>
2022-10-10 22:55:34 +0300

    fixed bugs, deleted excessive comments.

--- Services/EnergyCostAlgorithm.cs
+++ Services/EnergyCostAlgorithm.cs
@@ -3,32 +3,24 @@ namespace Ultra_Saver;
 
 public class EnergyCostAlgorithm
 {
-    public float TotalEnergy { get; set; } = 0;
+    public double TotalEnergy { get; set; } = 0;
 
     // powerScale is a proportion of power used for the specified time; scales 1 to 10; ex.: medium heat, 3/10 of power
     // if User's appliance power efficiancy is 80% then ApplianceEfficiency = 0.8
-    public void ElectricPower (short AppliancePower, byte PowerScale, short TimeInMin, float ApplianceEfficiency){
+    public void ElectricPower (int AppliancePower, short PowerScale, short TimeInMin, float ApplianceEfficiency){
         TotalEnergy += AppliancePower * PowerScale * TimeInMin / 600_000 / ApplianceEfficiency;
     }
 
-    public void GasPower (short AppliancePower, byte PowerScale, short TimeInMin, float ApplianceEfficiency){
-        // small stove - ~1200w , medium - ~1500 - 1800, large - up to 3k
-        // 1 kWh = 0.0947 m3 gas (100% efficient combustion)
-        //TotalEnergy +=  / ApplianceEfficiency
+    public void GasPower (int AppliancePower, short PowerScale, short TimeInMin, float ApplianceEfficiency){
+        TotalEnergy += AppliancePower * (float) 0.0947 / ApplianceEfficiency;
+    }
+
+    public void HeatingPan (int AppliancePower, float ApplianceEfficiency){
+        // considering heating pan is for approximately 2 minutes
+        TotalEnergy += AppliancePower / 108_000_000 / ApplianceEfficiency;
     }
 
     public void BoilingWater (short WaterVolume, float ApplianceEfficiency){
-        // If 100% efficiency
-        TotalEnergy += 4200 * WaterVolume * 1000 * 80 / ApplianceEfficiency;
-    }
-
-    public void HeatingPan (short AppliancePower, float ApplianceEfficiency){
-        // considering heating pan is for approximately 2 minutes
-        TotalEnergy += AppliancePower / 30 / ApplianceEfficiency;
+        TotalEnergy += (float) 0.336 / 3600 * WaterVolume / ApplianceEfficiency;
     }
 }
-
-// Research clearly shows that induction cooktops are more energy efficient:
-// gas cooktops are about 40 percent efficient;
-// electric-coil and standard smooth-top electric cooktops are about 74 percent efficient,
-// and induction cooktops are 84 percent efficient
