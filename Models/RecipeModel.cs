5492639e5e14f3f878927cad64f2afa6cb4d1936
Jurshenas <rokasjuren@gmail.com>
2022-10-12 10:50:05 +0300

    Added Table Models Added Model Relationships in AppDatabaseContext

--- Models/RecipeModel.cs
+++ Models/RecipeModel.cs
@@ -1,11 +1,28 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Ultra_Saver;
 public class RecipeModel
 {
+
+    [Key]
     public int Id { get; set; }
 
-    [Required]
-    public int Minutes { get; set; }
-    public int Wattage { get; set; }
+    public string Owner { get; set; }
+    public Owner Owner { get; set; }  // Navigation Properties
+
+    public string Instruction { get; set; }
+
+    public int CalorieCount { get; set; }
+
+    public int FullPrepTime { get; set; }
+
+    // Picture
+
+    // Navigation Properties
+    public List<Comment> Comments { get; set; }
+
+    public List<UserLikedRecipe> UserLikedRecipe { get; set; }
+
+    public List<RecipeIngredient> RecipeIngredient { get; set; }
 }
