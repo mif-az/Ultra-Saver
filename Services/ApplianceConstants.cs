bf617802c4005cf9da27d78975faa489ba30b294
Storas008 <naglis.kontautas04@gmail.com>
2022-10-12 13:48:08 +0300

    fixed bugs, made code more convenient to read

---
+++ Services/ApplianceConstants.cs
@@ -0,0 +1,19 @@
+namespace Ultra_Saver;
+
+public struct ApplianceConstants {
+        enum ApllianceEfficiency : int {
+            Induction = 84,
+            Coil = 74,
+            Gas = 40,
+            Microwave = 50,
+            Oven = 90,
+            Grill,
+            RiceCooker
+        }
+
+// Research clearly shows that induction cooktops are more energy efficient:
+// gas cooktops are about 40 percent efficient;
+// electric-coil and standard smooth-top electric cooktops are about 74 percent efficient,
+// and induction cooktops are 84 percent efficient
+
+}
--- Services/EnergyCostAlgorithm.cs
+++ Services/EnergyCostAlgorithm.cs
@@ -3,24 +3,30 @@ namespace Ultra_Saver;
 
 public class EnergyCostAlgorithm
 {
+    private const int ConvertTokWh = 3_600_000;
+    private const float kWhToGas = ConvertTokWh * (float) 0.0947;
+
+    //                              Q =   c  *  ρ  *    Δt ;  Litres -> Millilitres
+    private const int WaterToBoilHeat = 4200 * 997 * (100 - 20) / 1000;
     public double TotalEnergy { get; set; } = 0;
 
     // powerScale is a proportion of power used for the specified time; scales 1 to 10; ex.: medium heat, 3/10 of power
     // if User's appliance power efficiancy is 80% then ApplianceEfficiency = 0.8
-    public void ElectricPower (int AppliancePower, short PowerScale, short TimeInMin, float ApplianceEfficiency){
-        TotalEnergy += AppliancePower * PowerScale * TimeInMin / 600_000 / ApplianceEfficiency;
+    public void ElectricPower (int AppliancePower, short PowerScale, int Time, int ApplianceEfficiency){
+        TotalEnergy += AppliancePower * PowerScale * Time / ConvertTokWh / ApplianceEfficiency * 100;
     }
 
-    public void GasPower (int AppliancePower, short PowerScale, short TimeInMin, float ApplianceEfficiency){
-        TotalEnergy += AppliancePower * (float) 0.0947 / ApplianceEfficiency;
+    public void GasPower (int AppliancePower, short PowerScale, int Time, int ApplianceEfficiency){
+        TotalEnergy += AppliancePower * PowerScale * Time * kWhToGas / ApplianceEfficiency * 100;
     }
 
-    public void HeatingPan (int AppliancePower, float ApplianceEfficiency){
+    public void HeatingPan (int AppliancePower, int ApplianceEfficiency){
+        // If the recipe requires heating a pan
         // considering heating pan is for approximately 2 minutes
-        TotalEnergy += AppliancePower / 108_000_000 / ApplianceEfficiency;
+        TotalEnergy += AppliancePower * 120 / ConvertTokWh / ApplianceEfficiency * 100;
     }
 
-    public void BoilingWater (short WaterVolume, float ApplianceEfficiency){
-        TotalEnergy += (float) 0.336 / 3600 * WaterVolume / ApplianceEfficiency;
+    public void BoilingWater (short WaterVolume, int ApplianceEfficiency){
+        TotalEnergy += WaterToBoilHeat / ConvertTokWh * WaterVolume / ApplianceEfficiency * 100;
     }
 }
