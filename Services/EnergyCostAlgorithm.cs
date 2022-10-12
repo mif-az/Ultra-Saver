b6470731c98503db645a2a411dbb68dd6dd879c3
Storas008 <naglis.kontautas04@gmail.com>
2022-10-12 17:51:46 +0300

    added enum, struct, improve d algorithm

--- Services/EnergyCostAlgorithm.cs
+++ Services/EnergyCostAlgorithm.cs
@@ -4,7 +4,7 @@ namespace Ultra_Saver;
 public class EnergyCostAlgorithm
 {
     private const int ConvertTokWh = 3_600_000;
-    private const float kWhToGas = ConvertTokWh * (float) 0.0947;
+    private const float kWhToGas = ConvertTokWh * (float)0.0947;
 
     //                              Q =   c  *  ρ  *    Δt ;  Litres -> Millilitres
     private const int WaterToBoilHeat = 4200 * 997 * (100 - 20) / 1000;
@@ -12,21 +12,25 @@ public class EnergyCostAlgorithm
 
     // powerScale is a proportion of power used for the specified time; scales 1 to 10; ex.: medium heat, 3/10 of power
     // if User's appliance power efficiancy is 80% then ApplianceEfficiency = 0.8
-    public void ElectricPower (int AppliancePower, short PowerScale, int Time, int ApplianceEfficiency){
-        TotalEnergy += AppliancePower * PowerScale * Time / ConvertTokWh / ApplianceEfficiency * 100;
+    public void ElectricPower(int AppliancePower, short PowerScale, int Time, ApplianceType Type)
+    {
+        TotalEnergy += AppliancePower * PowerScale * Time / ConvertTokWh / ApplianceEfficiency.GetEfficiency(Type);
     }
 
-    public void GasPower (int AppliancePower, short PowerScale, int Time, int ApplianceEfficiency){
-        TotalEnergy += AppliancePower * PowerScale * Time * kWhToGas / ApplianceEfficiency * 100;
+    public void GasPower(int AppliancePower, short PowerScale, int Time, ApplianceType Type)
+    {
+        TotalEnergy += AppliancePower * PowerScale * Time * kWhToGas / ApplianceEfficiency.GetEfficiency(Type);
     }
 
-    public void HeatingPan (int AppliancePower, int ApplianceEfficiency){
+    public void HeatingPan(int AppliancePower, ApplianceType Type)
+    {
         // If the recipe requires heating a pan
         // considering heating pan is for approximately 2 minutes
-        TotalEnergy += AppliancePower * 120 / ConvertTokWh / ApplianceEfficiency * 100;
+        TotalEnergy += AppliancePower * 120 / ConvertTokWh / ApplianceEfficiency.GetEfficiency(Type);
     }
 
-    public void BoilingWater (short WaterVolume, int ApplianceEfficiency){
-        TotalEnergy += WaterToBoilHeat / ConvertTokWh * WaterVolume / ApplianceEfficiency * 100;
+    public void BoilingWater(short WaterVolume, ApplianceType Type)
+    {
+        TotalEnergy += WaterToBoilHeat / ConvertTokWh * WaterVolume / ApplianceEfficiency.GetEfficiency(Type);
     }
 }
