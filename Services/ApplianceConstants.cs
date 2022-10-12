d5bf2ba75f88c1ee585914ca9dcdb0bf75b74001
Storas008 <naglis.kontautas04@gmail.com>
2022-10-12 13:49:28 +0300

    a

--- Services/ApplianceConstants.cs
+++ Services/ApplianceConstants.cs
@@ -10,10 +10,4 @@ public struct ApplianceConstants {
             Grill,
             RiceCooker
         }
-
-// Research clearly shows that induction cooktops are more energy efficient:
-// gas cooktops are about 40 percent efficient;
-// electric-coil and standard smooth-top electric cooktops are about 74 percent efficient,
-// and induction cooktops are 84 percent efficient
-
 }
