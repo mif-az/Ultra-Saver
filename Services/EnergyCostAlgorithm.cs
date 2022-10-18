55cd032ead1ea4b8930df4ae6523a4e99db0282b
Storas008 <naglis.kontautas04@gmail.com>
2022-10-18 15:56:41 +0300

    Filled requirement

--- Services/EnergyCostAlgorithm.cs
+++ Services/EnergyCostAlgorithm.cs
@@ -1,22 +1,23 @@
 
 namespace Ultra_Saver;
 
-public class EnergyCostAlgorithm
+public struct EnergyCostAlgorithm
 {
+    public EnergyCostAlgorithm() { }
+    public double TotalEnergy { get; set; } = 0;
+    public float GasVolumeUsed { get; set; } = 0;
+
     //                              hours * kilo
     private const int ConvertTokWh = 3_600_000;
 
     //                           1 kWh = 0.0947 m3 of natural gas
     private const float kWhGasCoefficient = (float)0.0947;
-    
+
     //                              Q =          ρ  *  c
     private const float GasBurnEnergy = (float)0.68 * 49_000_000;
 
     //                              Q =   c  *  ρ  *    Δt ;  Litres -> Millilitres (m3)
     private const int WaterToBoilHeat = 4200 * 997 * (100 - 20) / 1000;
-
-    public double TotalEnergy { get; set; } = 0;
-    public float GasVolumeUsed { get; set; } = 0;
 
     // powerScale is a proportion of power used for the specified time. scales 0.1 to 1
     // if User's appliance power efficiancy is 80% then ApplianceEfficiency = 0.8
@@ -25,12 +26,12 @@ public class EnergyCostAlgorithm
         TotalEnergy += AppliancePower * PowerScale * Time / ConvertTokWh / ApplianceEfficiency.GetEfficiency(Type);
     }
 
-    public float kWhConvertGas(float TotalEnergyUsed)
+    public static float kWhConvertGas(float TotalEnergyUsed)
     {
         return TotalEnergyUsed * kWhGasCoefficient / ApplianceEfficiency.GetEfficiency(ApplianceType.GAS_STOVE);
     }
 
-    public float GasConvertkWh(float TotalGasUsed, ApplianceType Type)
+    public static float GasConvertkWh(float TotalGasUsed, ApplianceType Type)
     {
         return TotalGasUsed * GasBurnEnergy / ConvertTokWh / ApplianceEfficiency.GetEfficiency(Type);
     }
